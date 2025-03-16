ca1474480cc2b89082303982966cd322039bb609
neoguojing <guojing_neo@163.com>
2025-03-16 13:08:46 +0800

    finish graph test

--- tests/tasks/task_graph_test.py
+++ tests/tasks/task_graph_test.py
@@ -8,60 +8,140 @@ class TestAgent(unittest.TestCase):
         self.graph = AgiGraph()
         self.graph.display()
         
-    # def test_text(self):
-    #     input_example = {
-    #         "messages":  [
-    #             HumanMessage(content="俄乌局势")
-    #         ],
-    #         "input_type": "text",
-    #         "need_speech": False,
-    #         "status": "in_progress",
-    #     }
-    #     resp = self.graph.invoke(input_example)
-    #     print(resp["messages"][-1],AIMessage)
-    #     self.assertIsInstance(resp,dict)
-    #     self.assertIsInstance(resp["messages"],list)
-    #     self.assertEqual(resp["input_type"],"text")
-    #     self.assertIsInstance(resp["messages"][-1],AIMessage)
+    def test_text(self):
+        input_example = {
+            "messages":  [
+                HumanMessage(content="俄乌局势")
+            ],
+            "input_type": "text",
+            "need_speech": False,
+            "status": "in_progress",
+        }
+        resp = self.graph.invoke(input_example)
+        print(resp)
+        self.assertIsInstance(resp,dict)
+        self.assertIsInstance(resp["messages"],list)
+        self.assertEqual(resp["input_type"],"text")
+        self.assertIsInstance(resp["messages"][-1],AIMessage)
+        self.assertIsNotNone(resp["messages"][-1].content)
         
-    # def test_text_image_gene(self):
-    #     # 使用agent，由agent决策是否调用图片生成工具
-    #     input_example = {
-    #         "messages":  [
-    #             HumanMessage(
-    #                 content="生成一张超人拯救了太阳",
-    #             )
-    #         ],
-    #         "input_type": "text",
-    #         "need_speech": False,
-    #         "status": "in_progress",
-    #     }
-    #     resp = self.graph.invoke(input_example)
-    #     print(resp["messages"][-1])
-    #     self.assertIsInstance(resp,dict)
-    #     self.assertIsInstance(resp["messages"],list)
-    #     self.assertEqual(resp["input_type"],"text")
-    #     self.assertIsInstance(resp["messages"][-1],ToolMessage)
+        human_message_count = 0
+        tool_message_count = 0
+        ai_message_count = 0
+        events = self.graph.stream(input_example)
+        # TODO AIMessage 返回了两个，ToolMessage返回一个
+        for event in events:
+            print("******event******",event,type(event))
+            if isinstance(event,HumanMessage):
+                human_message_count += 1
+            elif isinstance(event,ToolMessage):
+                tool_message_count += 1
+                self.assertIsInstance(event.content,str)
+                self.assertIsNotNone(event.content)
+            elif isinstance(event,AIMessage):
+                ai_message_count += 1
+                # TODO 此处会返回两个AIMessage，业务侧处理会形成混淆
+                self.assertIsInstance(event.content,str)
+                self.assertIsNotNone(event.content)
+
+        # TODO tool_message_count 为什么会有两个
+        print("human_message_count:",human_message_count,"tool_message_count:",tool_message_count,"ai_message_count:",ai_message_count)
     
-    # def test_image_image_gene(self):
-    #     input_example = {
-    #         "messages":  [
-    #             HumanMessage(
-    #                 content=[
-    #                     {"type":"text","text":"猫咪是黑猫警长"},
-    #                     {"type":"image","image":"tests/cat.jpg"},
-    #                 ],
-    #             )
-    #         ],
-    #         "input_type": "image",
-    #         "need_speech": False,
-    #         "status": "in_progress",
-    #     }
-    #     resp = self.graph.invoke(input_example)
-    #     self.assertIsInstance(resp,dict)
-    #     self.assertIsInstance(resp["messages"],list)
-    #     self.assertEqual(resp["input_type"],"image")
-    #     self.assertIsInstance(resp["messages"][-1],AIMessage)
+        
+    def test_text_image_gene(self):
+        # 使用agent，由agent决策是否调用图片生成工具
+        input_example = {
+            "messages":  [
+                HumanMessage(
+                    content="生成一张超人拯救了太阳",
+                )
+            ],
+            "input_type": "text",
+            "need_speech": False,
+            "status": "in_progress",
+        }
+        resp = self.graph.invoke(input_example)
+        print(resp)
+        self.assertIsInstance(resp,dict)
+        self.assertIsInstance(resp["messages"],list)
+        self.assertEqual(resp["input_type"],"text")
+        self.assertIsInstance(resp["messages"][-1],ToolMessage)
+        self.assertIsInstance(resp["messages"][-1].content,list)
+        self.assertIsInstance(resp["messages"][-1].content[0],dict)
+        self.assertEqual(resp["messages"][-1].content[0].get("type"),"image")
+        self.assertIsNotNone(resp["messages"][-1].content[0].get("image"))
+        
+        human_message_count = 0
+        tool_message_count = 0
+        ai_message_count = 0
+        events = self.graph.stream(input_example)
+        # TODO 返回的首先是一个Aimessage ，然后返回一个Toolmessage
+        for event in events:
+            print("******event******",event,type(event))
+            if isinstance(event,HumanMessage):
+                human_message_count += 1
+            elif isinstance(event,ToolMessage):
+                tool_message_count += 1
+                self.assertIsInstance(event.content,list)
+                self.assertIsInstance(event.content[0],dict)
+                self.assertEqual(event.content[0].get("type"),"image")
+                self.assertIsNotNone(event.content[0].get("image"))
+            elif isinstance(event,AIMessage):
+                ai_message_count += 1
+                # TODO 此处会返回两个AIMessage，业务侧处理会形成混淆
+                # if isinstance(event.content,list):
+                # self.assertIsInstance(event.content,list)
+                # self.assertIsInstance(event.content[0],dict)
+                # self.assertEqual(event.content[0].get("type"),"image")
+                # self.assertIsNotNone(event.content[0].get("image"))
+        # TODO tool_message_count 为什么会有两个
+        print("human_message_count:",human_message_count,"tool_message_count:",tool_message_count,"ai_message_count:",ai_message_count)
+    
+    
+    def test_image_image_gene(self):
+        input_example = {
+            "messages":  [
+                HumanMessage(
+                    content=[
+                        {"type":"text","text":"猫咪是黑猫警长"},
+                        {"type":"image","image":"tests/cat.jpg"},
+                    ],
+                )
+            ],
+            "input_type": "image",
+            "need_speech": False,
+            "status": "in_progress",
+        }
+        resp = self.graph.invoke(input_example)
+        print(resp)
+        self.assertIsInstance(resp,dict)
+        self.assertIsInstance(resp["messages"],list)
+        self.assertEqual(resp["input_type"],"image")
+        self.assertIsInstance(resp["messages"][-1],AIMessage)
+        self.assertIsInstance(resp["messages"][-1].content,list)
+        self.assertIsInstance(resp["messages"][-1].content[0],dict)
+        self.assertEqual(resp["messages"][-1].content[0].get("type"),"image")
+        self.assertIsNotNone(resp["messages"][-1].content[0].get("image"))
+        human_message_count = 0
+        tool_message_count = 0
+        ai_message_count = 0
+        events = self.graph.stream(input_example)
+        for event in events:
+            print("******event******",event,type(event))
+            if isinstance(event,HumanMessage):
+                human_message_count += 1
+            elif isinstance(event,ToolMessage):
+                tool_message_count += 1
+            elif isinstance(event,AIMessage):
+                ai_message_count += 1
+                # TODO 此处会返回两个AIMessage，业务侧处理会形成混淆
+                # if isinstance(event.content,list):
+                self.assertIsInstance(event.content,list)
+                self.assertIsInstance(event.content[0],dict)
+                self.assertEqual(event.content[0].get("type"),"image")
+                self.assertIsNotNone(event.content[0].get("image"))
+        # TODO tool_message_count 为什么会有两个
+        print("human_message_count:",human_message_count,"tool_message_count:",tool_message_count,"ai_message_count:",ai_message_count)
     
     def test_audio_input(self):
         # 语音输入，语音输出
@@ -102,6 +182,7 @@ class TestAgent(unittest.TestCase):
                 tool_message_count += 1
             elif isinstance(event,AIMessage):
                 ai_message_count += 1
+                # TODO 此处会返回两个AIMessage，业务侧处理会形成混淆
                 if isinstance(event.content,list):
                     self.assertIsInstance(event.content[0],dict)
                     self.assertEqual(event.content[0].get("type"),"audio")
@@ -171,92 +252,92 @@ class TestAgent(unittest.TestCase):
         print("human_message_count:",human_message_count,"tool_message_count:",tool_message_count,"ai_message_count:",ai_message_count)
 
         
-    # def test_web(self):
-    #     input_example = {
-    #         "messages":  [
-    #             HumanMessage(
-    #                 content="今天上海天气如何？"
-    #             )
-    #         ],
-    #         "input_type": "text",
-    #         "need_speech": False,
-    #         "feature": "web",
-    #     }
-    #     resp = self.graph.invoke(input_example)
-    #     print(resp)
-    #     self.assertIsInstance(resp,dict)
-    #     self.assertIsInstance(resp["messages"],list)
-    #     self.assertEqual(resp["feature"],"web")
-    #     self.assertEqual(resp["input_type"],"text")
-    #     self.assertIsInstance(resp["messages"][-1],AIMessage)
-    #     self.assertIsInstance(resp["messages"][-1].additional_kwargs['context'],list)
-    #     self.assertIsNotNone(resp["messages"][-1].content)
-    #     self.assertIsInstance(resp["messages"][-1].additional_kwargs['citations'],list)
+    def test_web(self):
+        input_example = {
+            "messages":  [
+                HumanMessage(
+                    content="今天上海天气如何？"
+                )
+            ],
+            "input_type": "text",
+            "need_speech": False,
+            "feature": "web",
+        }
+        resp = self.graph.invoke(input_example)
+        print(resp)
+        self.assertIsInstance(resp,dict)
+        self.assertIsInstance(resp["messages"],list)
+        self.assertEqual(resp["feature"],"web")
+        self.assertEqual(resp["input_type"],"text")
+        self.assertIsInstance(resp["messages"][-1],AIMessage)
+        self.assertIsInstance(resp["messages"][-1].additional_kwargs['context'],list)
+        self.assertIsNotNone(resp["messages"][-1].content)
+        self.assertIsInstance(resp["messages"][-1].additional_kwargs['citations'],list)
         
-    #     human_message_count = 0
-    #     tool_message_count = 0
-    #     ai_message_count = 0
-    #     events = self.graph.stream(input_example)
-    #     for event in events:
-    #         print("******event******",event,type(event))
-    #         if isinstance(event,HumanMessage):
-    #             human_message_count += 1
-    #         elif isinstance(event,ToolMessage):
-    #             tool_message_count += 1
-    #             self.assertIsInstance(event.additional_kwargs['context'],list)
-    #             self.assertIsInstance(event.additional_kwargs['citations'],list)
-    #             self.assertEqual(event.content,"working...")
-    #             self.assertEqual(event.tool_call_id,"web or rag")
-    #         elif isinstance(event,AIMessage):
-    #             ai_message_count += 1
-    #             self.assertIsInstance(event.additional_kwargs['context'],list)
-    #             self.assertIsInstance(event.additional_kwargs['citations'],list)
-    #             self.assertIsNotNone(event.content)
-    #     # TODO tool_message_count 为什么会有两个
-    #     print("human_message_count:",human_message_count,"tool_message_count:",tool_message_count,"ai_message_count:",ai_message_count)
+        human_message_count = 0
+        tool_message_count = 0
+        ai_message_count = 0
+        events = self.graph.stream(input_example)
+        for event in events:
+            print("******event******",event,type(event))
+            if isinstance(event,HumanMessage):
+                human_message_count += 1
+            elif isinstance(event,ToolMessage):
+                tool_message_count += 1
+                self.assertIsInstance(event.additional_kwargs['context'],list)
+                self.assertIsInstance(event.additional_kwargs['citations'],list)
+                self.assertEqual(event.content,"working...")
+                self.assertEqual(event.tool_call_id,"web or rag")
+            elif isinstance(event,AIMessage):
+                ai_message_count += 1
+                self.assertIsInstance(event.additional_kwargs['context'],list)
+                self.assertIsInstance(event.additional_kwargs['citations'],list)
+                self.assertIsNotNone(event.content)
+        # TODO tool_message_count 为什么会有两个
+        print("human_message_count:",human_message_count,"tool_message_count:",tool_message_count,"ai_message_count:",ai_message_count)
 
-    # def test_custom_rag(self):
-    #     import json
-    #     input_example = {
-    #         "messages":  [
-    #             HumanMessage(
-    #                 content="NTP3000Plus",
-    #                 additional_kwargs={"collection_names":json.dumps(["test"])}
-    #             )
-    #         ],
-    #         "input_type": "text",
-    #         "need_speech": False,
-    #         "feature": "rag",
-    #     }
-    #     resp = self.graph.invoke(input_example)
-    #     print("resp:\n",resp)
-    #     self.assertIsInstance(resp,dict)
-    #     self.assertIsInstance(resp["messages"],list)
-    #     self.assertEqual(resp["feature"],"rag")
-    #     self.assertEqual(resp["input_type"],"text")
-    #     self.assertIsInstance(resp["messages"][-1],AIMessage)
-    #     self.assertIsInstance(resp["messages"][-1].additional_kwargs['context'],list)
-    #     self.assertIsNotNone(resp["messages"][-1].content)
-    #     self.assertIsInstance(resp["messages"][-1].additional_kwargs['citations'],list)
+    def test_custom_rag(self):
+        import json
+        input_example = {
+            "messages":  [
+                HumanMessage(
+                    content="NTP3000Plus",
+                    additional_kwargs={"collection_names":json.dumps(["test"])}
+                )
+            ],
+            "input_type": "text",
+            "need_speech": False,
+            "feature": "rag",
+        }
+        resp = self.graph.invoke(input_example)
+        print("resp:\n",resp)
+        self.assertIsInstance(resp,dict)
+        self.assertIsInstance(resp["messages"],list)
+        self.assertEqual(resp["feature"],"rag")
+        self.assertEqual(resp["input_type"],"text")
+        self.assertIsInstance(resp["messages"][-1],AIMessage)
+        self.assertIsInstance(resp["messages"][-1].additional_kwargs['context'],list)
+        self.assertIsNotNone(resp["messages"][-1].content)
+        self.assertIsInstance(resp["messages"][-1].additional_kwargs['citations'],list)
         
-    #     human_message_count = 0
-    #     tool_message_count = 0
-    #     ai_message_count = 0
-    #     events = self.graph.stream(input_example)
-    #     for event in events:
-    #         print("******event******",event,type(event))
-    #         if isinstance(event,HumanMessage):
-    #             human_message_count += 1
-    #         elif isinstance(event,ToolMessage):
-    #             tool_message_count += 1
-    #             self.assertIsInstance(event.additional_kwargs['context'],list)
-    #             self.assertIsInstance(event.additional_kwargs['citations'],list)
-    #             self.assertEqual(event.content,"working...")
-    #             self.assertEqual(event.tool_call_id,"web or rag")
-    #         elif isinstance(event,AIMessage):
-    #             ai_message_count += 1
-    #             self.assertIsInstance(event.additional_kwargs['context'],list)
-    #             self.assertIsInstance(event.additional_kwargs['citations'],list)
-    #             self.assertIsNotNone(event.content)
-    #     # TODO tool_message_count 为什么会有两个
-    #     print("human_message_count:",human_message_count,"tool_message_count:",tool_message_count,"ai_message_count:",ai_message_count)
+        human_message_count = 0
+        tool_message_count = 0
+        ai_message_count = 0
+        events = self.graph.stream(input_example)
+        for event in events:
+            print("******event******",event,type(event))
+            if isinstance(event,HumanMessage):
+                human_message_count += 1
+            elif isinstance(event,ToolMessage):
+                tool_message_count += 1
+                self.assertIsInstance(event.additional_kwargs['context'],list)
+                self.assertIsInstance(event.additional_kwargs['citations'],list)
+                self.assertEqual(event.content,"working...")
+                self.assertEqual(event.tool_call_id,"web or rag")
+            elif isinstance(event,AIMessage):
+                ai_message_count += 1
+                self.assertIsInstance(event.additional_kwargs['context'],list)
+                self.assertIsInstance(event.additional_kwargs['citations'],list)
+                self.assertIsNotNone(event.content)
+        # TODO tool_message_count 为什么会有两个
+        print("human_message_count:",human_message_count,"tool_message_count:",tool_message_count,"ai_message_count:",ai_message_count)
