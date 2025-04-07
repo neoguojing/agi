f9b0a4f85304ff19ccc8ab7ee71277a364e436bd
guoerjun <guoerjun@sensetime.com>
2025-04-07 19:58:36 +0800

    finished the multimodel test except video input

--- tests/llms/test_multimodel.py
+++ tests/llms/test_multimodel.py
@@ -11,26 +11,35 @@ class TestMultiModel(unittest.TestCase):
         cls.instance = MultiModel()
 
 
-    # def test_text_input(self):
-    #     input = HumanMessage(content="介绍下多模态模型")
-    #     output = self.__class__.instance.invoke(input)
-    #     self.assertIsNotNone(output)
-    #     self.assertIsNotNone(output.content)
-    #     print(output.content)
+    def test_text_input(self):
+        input = HumanMessage(content="介绍下多模态模型")
+        output = self.__class__.instance.invoke(input)
+        self.assertIsNotNone(output)
+        self.assertIsNotNone(output.content)
+        self.assertIsInstance(output.content,list)
+        self.assertEqual(output.content[0].get("type"),"text")
+        self.assertIsNotNone(output.content[0].get("text"))
+        print(output.content)
 
-    # def test_audio_input(self):
-    #     input = HumanMessage(content=[{"type":"audio","audio":"tests/zh-cn-sample.wav"}])
-    #     output = self.__class__.instance.invoke(input)
-    #     self.assertIsNotNone(output)
-    #     self.assertIsNotNone(output.content)
-    #     print(output.content)
+    def test_audio_input(self):
+        input = HumanMessage(content=[{"type":"audio","audio":"tests/zh-cn-sample.wav"}])
+        output = self.__class__.instance.invoke(input)
+        self.assertIsNotNone(output)
+        self.assertIsNotNone(output.content)
+        self.assertIsInstance(output.content,list)
+        self.assertEqual(output.content[0].get("type"),"text")
+        self.assertIsNotNone(output.content[0].get("text"))
+        print(output.content)
 
-    # def test_image_input(self):
-    #     input = HumanMessage(content=[{"type":"image","image":"tests/cat.jpg"}])
-    #     output = self.__class__.instance.invoke(input)
-    #     self.assertIsNotNone(output)
-    #     self.assertIsNotNone(output.content)
-    #     print(output.content)
+    def test_image_input(self):
+        input = HumanMessage(content=[{"type":"image","image":"tests/cat.jpg"}])
+        output = self.__class__.instance.invoke(input)
+        self.assertIsNotNone(output)
+        self.assertIsNotNone(output.content)
+        self.assertIsInstance(output.content,list)
+        self.assertEqual(output.content[0].get("type"),"text")
+        self.assertIsNotNone(output.content[0].get("text"))
+        print(output.content)
 
     def test_audio_output(self):
         config={"configurable": {"need_speech": True}}
@@ -38,6 +47,9 @@ class TestMultiModel(unittest.TestCase):
         output = self.__class__.instance.invoke(input,config=config)
         self.assertIsNotNone(output)
         self.assertIsNotNone(output.content)
+        self.assertIsInstance(output.content,list)
+        self.assertEqual(output.content[0].get("type"),"audio")
+        self.assertIsNotNone(output.content[0].get("audio"))
         print(output.content)
 
     # def test_video_input(self):
