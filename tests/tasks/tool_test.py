547fc25227948f8a18801bf0c9fe9f01d0aa7e7f
guoerjun <guoerjun@sensetime.com>
2025-04-23 17:27:47 +0800

    Add tool test

---
+++ tests/tasks/tool_test.py
@@ -0,0 +1,42 @@
+import unittest
+from agi.tasks.tools import (
+    wikidata,
+    wikipedia,
+    pythonREPL,
+)
+class TestTools(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_yahoo_news(self):
+        from langchain_community.tools.yahoo_finance_news import YahooFinanceNewsTool
+
+        tool = YahooFinanceNewsTool()
+        resp = tool.invoke("综合财经新闻")
+        print(resp)
+
+
+    def test_youtube(self):
+        from langchain_community.tools import YouTubeSearchTool
+
+        tool = YouTubeSearchTool()
+        resp = tool.invoke("llm")
+        print(resp)
+
+    def test_wikidata(self):
+
+        tool = wikidata()
+        resp = tool.invoke("Alan Turing")
+        print(resp)
+
+    def test_wikipedia(self):
+
+        tool = wikipedia()
+        resp = tool.invoke("姚明")
+        print(resp)
+
+    def test_pythonREPL(self):
+
+        tool = pythonREPL()
+        resp = tool.invoke("print(1+1)")
+        print(resp)
