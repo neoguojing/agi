b84c28ed1948a5987860298ea724906f9d591891
guoerjun <guoerjun@sensetime.com>
2025-05-23 15:59:19 +0800

    add wave and update cosyvoice depend

---
+++ tools/wave_gene.py
@@ -0,0 +1,13 @@
+# import sys
+# sys.path.append('third_party/Matcha-TTS')
+from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
+from cosyvoice.utils.file_utils import load_wav
+import torchaudio
+
+cosyvoice = CosyVoice2('/data/model/cosyvoice/CosyVoice2-0.5B', load_jit=False, load_trt=False, fp16=False, use_flow_cache=False)
+
+# NOTE if you want to reproduce the results on https://funaudiollm.github.io/cosyvoice2, please add text_frontend=False during inference
+# zero_shot usage
+prompt_speech_16k = load_wav('./asset/zero_shot_prompt.wav', 16000)
+for i, j in enumerate(cosyvoice.inference_zero_shot('你好小派', prompt_speech_16k, stream=False)):
+    torchaudio.save('./asset/wakeup_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
