a3182a025500e5862808572c6851713ef6fd30de
guoerjun <guoerjun@sensetime.com>
2025-05-23 17:46:22 +0800

    test cosy voice

--- tools/wave_gene.py
+++ tools/wave_gene.py
@@ -9,5 +9,5 @@ cosyvoice = CosyVoice2('/data/model/cosyvoice/CosyVoice2-0.5B', load_jit=False, 
 # NOTE if you want to reproduce the results on https://funaudiollm.github.io/cosyvoice2, please add text_frontend=False during inference
 # zero_shot usage
 prompt_speech_16k = load_wav('./asset/zero_shot_prompt.wav', 16000)
-for i, j in enumerate(cosyvoice.inference_zero_shot('你好小派', prompt_speech_16k, stream=False)):
+for i, j in enumerate(cosyvoice.inference_zero_shot('你好小派','希望你以后能够做的比我还好呦。', prompt_speech_16k, stream=False)):
     torchaudio.save('./asset/wakeup_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
