d02a73bc68bcf2dc4d1fe3cf12851c727e61c421
guoerjun <guoerjun@sensetime.com>
2025-05-30 18:02:29 +0800

    fix audio api test

--- tests/fast_api_audio_test.py
+++ tests/fast_api_audio_test.py
@@ -1,12 +1,9 @@
-import os
-import uuid
-import shutil
+
 from fastapi.testclient import TestClient
-from fastapi import HTTPException
-import pytest
 from agi.fastapi_agi import app
 from agi.config import log
-
+from agi.llms.tts import TextToSpeech 
+import numpy as np
 
 # 创建测试客户端
 client = TestClient(app)
@@ -14,6 +11,11 @@ client = TestClient(app)
 # 测试列出文件 API
 def test_audio_stream():
     tenant_id = "test_tenant"
+
+    dummy_pcm = np.ones(1024, dtype=np.int16)
+    TextToSpeech.get_queue(tenant_id).put(dummy_pcm.tobytes())
+
+
     response = client.get(f"/v1/audio_stream/{tenant_id}")
     assert response.status_code == 200
     assert response.headers["Content-Type"] == "audio/L16; rate=24000; channels=1"
@@ -24,10 +26,10 @@ def test_audio_stream():
         print(len(chunk))
         print(chunk)
 
-    #     assert isinstance(chunk, bytes)
-    #     assert len(chunk) == 2048
-    #     chunk_count += 1
-    #     if chunk_count >= 2:
-    #         break
+        assert isinstance(chunk, bytes)
+        assert len(chunk) == 2048
+        chunk_count += 1
+        if chunk_count >= 2:
+            break
 
-    # assert chunk_count >= 1
+    assert chunk_count >= 1
