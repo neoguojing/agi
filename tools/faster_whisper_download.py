9e7f6070a325a5fc45cca333aca448d44890b3b7
guoerjun <guoerjun@sensetime.com>
2025-06-18 19:31:08 +0800

    fix

--- tools/faster_whisper_download.py
+++ tools/faster_whisper_download.py
@@ -1,7 +1,8 @@
-from faster_whisper import WhisperModel
-
-# GPU: 8-bit 编码 + 混合精度解码
-model = WhisperModel("large-v3-turbo", device="cuda", compute_type="int8",download_root="./")
+from tools.faster_whisper_download import WhisperModel
 
 # CPU: 优化 8-bit
 model_cpu = WhisperModel("base", device="cpu", compute_type="int8",download_root="./")
+
+# GPU: 8-bit 编码 + 混合精度解码
+model = WhisperModel("large-v3", device="cuda", compute_type="int8",download_root="./")
+
