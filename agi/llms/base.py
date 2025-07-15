92e581c8393cc750d2a62588b10f646f31a35217
guoerjun <guoerjun@sensetime.com>
2025-07-15 15:39:29 +0800

    up

--- agi/llms/base.py
+++ agi/llms/base.py
@@ -36,6 +36,8 @@ def parse_input_messages(input: Union[HumanMessage,list[HumanMessage]]):
     
     from pathlib import Path
     def is_relative_path(path_str: str) -> bool:
+        if not any(sep in path_str for sep in (os.sep, '/', '\\')) and not path_str.startswith('.'):
+            return False
         return not Path(path_str).is_absolute()
     
     if is_relative_path(media):
