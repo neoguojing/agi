56cd9f76fb66f3a6aca870a803719a5ddd9455ef
guoerjun <guoerjun@sensetime.com>
2025-07-22 19:29:50 +0800

    fix

--- agi/apps/multimodal/multi_modal.py
+++ agi/apps/multimodal/multi_modal.py
@@ -57,14 +57,14 @@ class MultiModel:
             from transformers import Qwen2_5OmniForConditionalGeneration, Qwen2_5OmniProcessor
             self.model = Qwen2_5OmniForConditionalGeneration.from_pretrained(
                 self.model_path,
-                torch_dtype="auto",
+                torch_dtype=self.best_torch_dtype(),
                 device_map="auto",
                 enable_audio_output=True,
                 attn_implementation="flash_attention_2"
             )
             self.processor = Qwen2_5OmniProcessor.from_pretrained(self.model_path)
         elif self.model_name == "gemma":
-            # GPU 11GB
+            # GPU 11GB 最高要20GB
             self.model_path = os.path.join(MODEL_PATH, "gemma-3n-E2B-it")
             from transformers import AutoProcessor, Gemma3nForConditionalGeneration
             # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -169,6 +169,20 @@ class MultiModel:
             log.error(e)
             print(traceback.format_exc())
 
+    def best_torch_dtype(self):
+        if not torch.cuda.is_available():
+            return torch.float32  # CPU 上只能用 float32
+
+        device = torch.cuda.current_device()
+        major, _ = torch.cuda.get_device_capability(device)
+
+        if major >= 8:
+            return torch.bfloat16  # Ampere及以上，支持 BF16，优先使用
+        elif major >= 7:
+            return torch.float16   # Volta/Turing 支持 FP16
+        else:
+            return torch.float32   # 老架构，回退到 FP32
+        
     def _unload(self):
         print(f"[Model] Unloading model from {self.model_path}")
         del self.model
