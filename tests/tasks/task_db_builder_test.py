6fa32b16380bf6c705a27a7d871a19aa1f0393c9
guoerjun <guoerjun@sensetime.com>
2025-08-04 14:49:13 +0800

    ip

--- tests/tasks/task_db_builder_test.py
+++ tests/tasks/task_db_builder_test.py
@@ -13,7 +13,7 @@ async def test_db_graph():
     state = State()
     state['user_id'] = "ragtest"
     state['collection_name'] = "ragtest"
-    state['file_path'] = "tests/test.pdf"
+    state['file_path'] = "tests/2412.19437v1.pdf"
     ret = await db_graph.ainvoke(state,config=config)
     assert ret is not None, "ainvoke 返回值为空"
     colects = collection_manager.list_collections(tenant="ragtest")
