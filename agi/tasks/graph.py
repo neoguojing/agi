116260c3a92c93bf138e63a849ee5dc679dc6de4
guoerjun <guoerjun@sensetime.com>
2025-08-18 16:38:33 +0800

    fix issues

--- agi/tasks/graph.py
+++ agi/tasks/graph.py
@@ -262,7 +262,7 @@ class AgiGraph:
                     # TODO decide chain 和 tranlate chain 以及 web search chain会输出中间结果,需要想办法处理
                     if (isinstance(event[1][0],AIMessage)) and event[1][0].content:
                         meta = event[1][1]
-                        if meta.get("langgraph_node") in ["web","__start__","rag",'user_understand',"compress","intend"]:
+                        if meta.get("langgraph_node") in ["web","__start__","rag",'user_understand',"compress","intend","index_search","web"]:
                             continue
                         else:
                             # 某些场景下，如agent，返回消息非流式返回，整体作为一个返回：
