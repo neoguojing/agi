266574ca0453eab1dcb19f00a94d61de920bc05c
guoerjun <guoerjun@sensetime.com>
2026-03-19 11:15:05 +0800

    Delete

--- tests/agents_runtime/test_system_tools.py
+++
@@ -1,7 +0,0 @@
-from agi.agents_runtime.system_tools import execute_shell
-
-
-def test_execute_shell_runs_command():
-    result = execute_shell("echo hello")
-    assert result["exit_code"] == 0
-    assert "hello" in result["stdout"]
