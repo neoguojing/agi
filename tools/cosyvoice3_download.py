32a1bc89a36b14fb894fb04f1ab147ceeb147302
guoerjun <guoerjun@sensetime.com>
2026-03-31 18:05:36 +0800

    up

--- tools/cosyvoice3_download.py
+++ tools/cosyvoice3_download.py
@@ -1,2 +1,17 @@
 from huggingface_hub import snapshot_download
 snapshot_download('FunAudioLLM/Fun-CosyVoice3-0.5B-2512', local_dir='/data/model/Fun-CosyVoice3-0.5B')
+# -----------------------------
+# 下载 LLM 模型（文本 → audio token）
+# -----------------------------
+snapshot_download(
+    'yuekai/Fun-CosyVoice3-0.5B-2512-LLM-HF',
+    local_dir='/data/model/hf_cosyvoice3_llm'
+)
+
+# -----------------------------
+# 下载 FP16 ONNX 模型（token → wav）
+# -----------------------------
+snapshot_download(
+    'yuekai/Fun-CosyVoice3-0.5B-2512-FP16-ONNX',
+    local_dir='/data/model/Fun-CosyVoice3-0.5B-ONNX'
+)
